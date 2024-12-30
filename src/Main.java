60790e817ce45a95558b763d13bc6802023406a8
Patricia82TI <patricia.melloti@gmail.com>
2024-12-30 08:43:19 -0300

    Exercicio 1113 URI Online

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+//Leia uma quantidade indeterminada de duplas de valores inteiros X e Y. Escreva para cada X e Y uma mensagem que indique se estes valores foram digitados em ordem crescente ou decrescente.
+//
+//Entrada
+//A entrada contém vários casos de teste. Cada caso contém dois valores inteiros X e Y. A leitura deve ser encerrada ao ser fornecido valores iguais para X e Y.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        while(x != y){
+            if(x < y){
+                System.out.println("Crescente");
+            }else {
+                System.out.println("Decrescente");
+            }
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+    }
+}
